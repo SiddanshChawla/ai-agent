0de14768a790c26584212fd5355eb2d2355694ff
Siddansh Chawla <chawlasiddansh@gmail.com>
2024-01-10 23:21:56 +0530

    streamlit interface

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
 import requests
-from clarifai.client.model import Model
 from langchain_community.llms import Clarifai
-from langchain.agents import initialize_agent, load_tools, Tool, create_structured_chat_agent
-from langchain.chains import LLMChain
+from langchain.agents import initialize_agent, Tool
 from langchain.prompts import PromptTemplate
 from langchain_community.tools import DuckDuckGoSearchRun
+import streamlit as st
+import os
 
-USER_ID = 'openai'
-APP_ID = 'chat-completion'
-PAT = '9c5de2311a9f4ee998f1a28a9bfea9d5'
-MODEL_ID = 'gpt-4-turbo'
-MODEL_VERSION_ID = '182136408b4b4002a920fd500839f2c8'
-RAW_TEXT = 'Who won the worldcup cricket match in 2023?'
+# UI
+st.title("Need an LLM with internet?")
+user_input = st.text_input("Enter your query...")
 
-clarifai_llm = Clarifai(pat=PAT, user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID)
+clarifai_llm = Clarifai(pat=st.secrets["PAT"], user_id=st.secrets["USER_ID"], app_id=st.secrets["APP_ID"], model_id=st.secrets["MODEL_ID"])
 
 prompt = PromptTemplate(
     input_variables=["query"],
     template = "You are New Native Internal Bot. Help users with their important tasks, like a professor in a particular field. Query: {query}"
 )
-
-user_input = input("Enter the question: ")
-
-# llm_chain = LLMChain(llm = clarifai_llm, prompt=prompt)
-# print(llm_chain.invoke(f"{user_input}")['text'])
 
 webSearch = DuckDuckGoSearchRun()
 
@@ -41,5 +33,9 @@ agent = initialize_agent(
     max_iterations=3
 )
 
-r_1 = agent(f"{user_input}")
-print(f"Final answer: {r_1['output']}")
+if st.button('search'):
+    with st.spinner("Loading"):
+        r_1 = agent(f"{user_input}")
+        st.write(f"Final answer: {r_1['output']}")
+
+st.divider()
